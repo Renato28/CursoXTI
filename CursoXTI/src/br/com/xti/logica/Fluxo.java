89906a727ca4065f9ec9485d99d4ee6c57004f68
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 17:02:13 -0300

    inserindo comentarios

--- CursoXTI/src/br/com/xti/logica/Fluxo.java
+++ CursoXTI/src/br/com/xti/logica/Fluxo.java
@@ -1,11 +1,17 @@
 package br.com.xti.logica;
-
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Fluxo {
 
 	public static void main(String[] args) {
-
+		
+		//criando um array boolean bidimensional
 		boolean[][] matrix = { { false, true, false, false, false }, { false, false, false, false, false } };
 		
+		//criando e executando rotulos com a estrutural codicional for
 		busca:
 		for(int a = 0; a < matrix.length; a++) {
 			
@@ -13,8 +19,10 @@ public class Fluxo {
 			
 			for(int b = 0; b < matrix[a].length; b++) {
 				
+				//criando estrutura codicional if
 				if(matrix[a][b]) {
 					
+					//executando a condicional if
 					System.out.println("TRUE");
 					continue busca;
 				}
