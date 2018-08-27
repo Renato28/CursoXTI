c7a89f3ba99517561c37505543eb5eeb798e0a00
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 14:46:22 -0300

    criacao do metodo construtor

--- CursoXTI/src/br/com/xti/logica/AtalhoEclipse.java
+++ CursoXTI/src/br/com/xti/logica/AtalhoEclipse.java
@@ -1,6 +1,13 @@
 package br.com.xti.logica;
 
 public class AtalhoEclipse {
+	
+	@SuppressWarnings("unused")
+	//criacao do metodo construtor
+	
+	public AtalhoEclipse() {
+		
+	}
 
 	public static void main(String[] args) {
 		
