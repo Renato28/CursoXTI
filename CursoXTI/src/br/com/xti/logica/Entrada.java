33cf236917fe3e5191ca1b0b3fd5f97fa8f60ded
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 16:46:47 -0300

    inserindo comentarios

--- CursoXTI/src/br/com/xti/logica/Entrada.java
+++ CursoXTI/src/br/com/xti/logica/Entrada.java
@@ -2,14 +2,27 @@ package br.com.xti.logica;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class Entrada {
 
 	public static void main(String[] args) {
 		
+		//codigo comentado
 		
 		//System.out.println(args[0]);
+		
+		//criando a classe Scanner
 		Scanner s = new Scanner(System.in);
+		
+		//codigo comentado
+		
 		//System.out.println(s.nextLine());
+		
+		//criacao e execucao da variavel
 		
 		System.out.println("Qual é o seu nome?");
 		String nome = s.nextLine();
