3061f374067028a21442b1d8863d8261c0cb5d54
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 15:36:11 -0300

    inserindo comentarios

--- CursoXTI/src/br/com/xti/logica/ArraySimples.java
+++ CursoXTI/src/br/com/xti/logica/ArraySimples.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class ArraySimples {
 
 	public static void main(String[] args) {
