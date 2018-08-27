6ce10d0ed4f1924d9467bc5679542a41bd4416da
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 15:07:02 -0300

    criando uma variavel, incrementando, instanciando e mostrando o valor

--- CursoXTI/src/br/com/xti/logica/AutoBoxing.java
+++ CursoXTI/src/br/com/xti/logica/AutoBoxing.java
@@ -5,6 +5,10 @@ public class AutoBoxing {
 	public static void main(String[] args) {
 		
 		Integer x = new Integer(555); //empacotado
+		int a = x.intValue(); //desempacotar
+		a++; //incrementa
+		x = new Integer(a); //re-empacota
+		System.out.println(x.intValue());
 	}
 
 }
