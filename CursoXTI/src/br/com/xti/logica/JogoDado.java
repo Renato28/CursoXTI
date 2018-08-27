d32e48c6b8eb370a8d64f2018fcb649391622024
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 17:44:36 -0300

    criacao da classe Scanner e variavel para receber o palpite do usuario

--- CursoXTI/src/br/com/xti/logica/JogoDado.java
+++ CursoXTI/src/br/com/xti/logica/JogoDado.java
@@ -1,10 +1,14 @@
 package br.com.xti.logica;
 
+import java.util.Scanner;
+
 public class JogoDado {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Qual o seu palpite?");
+		int palpite = entrada.nextInt();
 	}
 
 }
