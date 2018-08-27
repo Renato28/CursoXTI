1ba150debdbd08d26054062a4fbc68a3d767e6d2
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 16:24:03 -0300

    criando e executando o valor da variavel

--- CursoXTI/src/br/com/xti/logica/Entrada.java
+++ CursoXTI/src/br/com/xti/logica/Entrada.java
@@ -9,6 +9,10 @@ public class Entrada {
 		//System.out.println(args[0]);
 		Scanner s = new Scanner(System.in);
 		System.out.println(s.nextLine());
+		
+		System.out.println("Qual é o seu nome?");
+		String nome = s.nextLine();
+		System.out.println("Bem vindo " + nome);
 
 	}
 
