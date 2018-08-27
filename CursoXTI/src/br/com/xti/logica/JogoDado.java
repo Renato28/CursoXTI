96e2438405c17f2a2edfcc31ea8986c0ebceee42
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 17:50:43 -0300

    criacao da estrutural codicional if para mostrar a mensagem do resultado do palpite

--- CursoXTI/src/br/com/xti/logica/JogoDado.java
+++ CursoXTI/src/br/com/xti/logica/JogoDado.java
@@ -6,16 +6,25 @@ import java.util.Scanner;
 public class JogoDado {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Qual o seu palpite?");
 		int palpite = entrada.nextInt();
-		
+
 		Random r = new Random();
-		int dado = r.nextInt(6)+1;
-		
+		int dado = r.nextInt(6) + 1;
+
 		System.out.println("Palpite: " + palpite);
 		System.out.println("Dado: " + dado);
+
+		if (palpite == dado) {
+
+			System.out.println("Você acertou!");
+
+		} else {
+
+			System.out.println("Você errou!");
+		}
 	}
 
 }
