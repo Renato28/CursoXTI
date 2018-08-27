01e53445561151ecc41c24dd1f4d383a2ceaa2b8
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 16:21:05 -0300

    comentando codigo

--- CursoXTI/src/br/com/xti/logica/Entrada.java
+++ CursoXTI/src/br/com/xti/logica/Entrada.java
@@ -6,7 +6,7 @@ public class Entrada {
 
 	public static void main(String[] args) {
 		
-		System.out.println(args[0]);
+		//System.out.println(args[0]);
 		Scanner s = new Scanner(System.in);
 		System.out.println(s.nextLine());
 
