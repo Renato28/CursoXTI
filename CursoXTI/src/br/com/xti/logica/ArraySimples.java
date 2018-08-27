c6843efd3909a43912bc9c0428058efcf24c3f4b
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 12:45:30 -0300

    criacao de um array de numeros inteiros impares

--- CursoXTI/src/br/com/xti/logica/ArraySimples.java
+++ CursoXTI/src/br/com/xti/logica/ArraySimples.java
@@ -4,6 +4,12 @@ public class ArraySimples {
 
 	public static void main(String[] args) {
 		
+		int[] impares = new int[5];
+		impares[0] = 1;
+		impares[1] = 3;
+		impares[2] = 5;
+		impares[3] = 7;
+		impares[4] = 9;
 	}
 
 }
