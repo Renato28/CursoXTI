40d760157f05756f865d0e543e8182788ad613c3
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 17:23:03 -0300

    criacao das variaveis para receber o peso e altura

--- CursoXTI/src/br/com/xti/logica/IMC.java
+++ CursoXTI/src/br/com/xti/logica/IMC.java
@@ -1,9 +1,13 @@
 package br.com.xti.logica;
 
+import javax.swing.JOptionPane;
+
 public class IMC {
 
 	public static void main(String[] args) {
 		
+		String peso = JOptionPane.showInputDialog("Qual o seu peso em quilogramas?");
+		String altura = JOptionPane.showInputDialog("Qual sua altura em metros?");
 		
 	}
 
