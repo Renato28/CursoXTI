ea9145489704228e32f2d778e21f39c5839f7942
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 15:52:44 -0300

    inserindo comentarios

--- CursoXTI/src/br/com/xti/logica/DesafioFebonacci.java
+++ CursoXTI/src/br/com/xti/logica/DesafioFebonacci.java
@@ -2,15 +2,25 @@ package br.com.xti.logica;
 
 public class DesafioFebonacci {
 
+	/**
+	 * 
+	 * @author Renato Nobrega
+	 */
 	public static void main(String[] args) {
+		
+		//criacao de variaveis
 		
 		int anterior = 0;
 		int proximo = 1;
 		
+		//executando a variavel
+		
 		System.out.println(anterior);
 		
+		//criacao da estrutura codicional for
 		while(proximo < 50) {
 			
+			//execucao da estrutura codicional for
 			System.out.println(proximo);
 			proximo = proximo + anterior;
 			anterior = proximo - anterior;
