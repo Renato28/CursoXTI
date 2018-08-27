4853fa4c94515cb03d3ff9af1f89c8d777f7a151
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 16:20:16 -0300

    criando e executando a classe Scanner

--- CursoXTI/src/br/com/xti/logica/Entrada.java
+++ CursoXTI/src/br/com/xti/logica/Entrada.java
@@ -1,9 +1,14 @@
 package br.com.xti.logica;
 
+import java.util.Scanner;
+
 public class Entrada {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println(args[0]);
+		Scanner s = new Scanner(System.in);
+		System.out.println(s.nextLine());
 
 	}
 
