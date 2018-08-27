655387ad75c752cdf9578a4bbeea5fb0152fd6b0
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 17:18:13 -0300

    inserindo comentarios

--- CursoXTI/src/br/com/xti/logica/HelloWord.java
+++ CursoXTI/src/br/com/xti/logica/HelloWord.java
@@ -1,9 +1,15 @@
 package br.com.xti.logica;
 
+/**
+ * 
+ * @author Renato Nobrega
+ *
+ */
 public class HelloWord {
 
 	public static void main(String[] args) {
 		
+		//execucao do metodo main
 		System.out.println("\tOla Mundo!");
 		System.out.println("Java \nSE");
 	}
