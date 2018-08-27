2c674892c504457973c2896453469217443abba7
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 14:48:33 -0300

    criando um array de inteiros

--- CursoXTI/src/br/com/xti/logica/AtalhoEclipse.java
+++ CursoXTI/src/br/com/xti/logica/AtalhoEclipse.java
@@ -12,6 +12,9 @@ public class AtalhoEclipse {
 		//criacao da classe Scanner
 		Scanner leitor = new Scanner(System.in);
 		leitor.nextLine();
+		
+		//criando um array de inteiros
+		int[] array = {1,2,3,4};
 	}
 
 	public static void main(String[] args) {
