5b7327da93a68679405775c005f0b952f039ff07
Renato Nobrega <renatonobrega87@gmail.com>
2018-08-27 16:51:20 -0300

    criando array boolean bidimensional

--- CursoXTI/src/br/com/xti/logica/Fluxo.java
+++ CursoXTI/src/br/com/xti/logica/Fluxo.java
@@ -3,7 +3,8 @@ package br.com.xti.logica;
 public class Fluxo {
 
 	public static void main(String[] args) {
-		
+
+		boolean[][] matrix = { { false, true, false, false, false }, { false, false, false, false, false } };
 		
 	}
 
